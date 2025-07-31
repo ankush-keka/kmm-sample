cbbfc5277c3f746bd8cec8cc7efc4e57e97f831c
Ankush <ankush.s@keka.com>
2025-07-31 17:31:55 +0530

    fix: Update XCFramework URL in Package.swift
    This commit updates the URL for the `shared` binary target in `Package.swift` to point to version `0.0.2-snapshot` of the release. The checksum remains unchanged.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "shared",
-            url: "https://github.com/ankush-keka/kmm-sample/releases/download/0.0.1-snapshot/shared.xcframework.zip",
+            url: "https://github.com/ankush-keka/kmm-sample/releases/download/0.0.2-snapshot/shared.xcframework.zip",
             checksum: "905e3130c425db89da231195f74b03cbfe5325ac60da50b4162db40e5626e01f"
         ),
     ]
